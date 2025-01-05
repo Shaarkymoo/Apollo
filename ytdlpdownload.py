fa83328d9fce00a5389be7b5d8d873daf7da7eef
Shaarky <shaaravagarwal@gmail.com>
2025-01-06 04:02:10 +0530

    Auto-commit: 2025-01-06 04:02:10

--- ytdlpdownload.py
+++ ytdlpdownload.py
@@ -7,4 +7,5 @@ with YoutubeDL() as ydl:
     ydl.download(urls)
     
 #command
-#git config --global user.email "you@example.com"
+# yt-dlp -P 'E:\Shaarav\wastebin\' -o '%(title)s - %(channel)s' -x --audio-format 'm4a' 
+# --ffmpeg-location 'C:\ffmpeg' https://www.youtube.com/playlist?list=PLIRFSUH_WlMrZhbyDNoK9o9mNW9XlPXs2
