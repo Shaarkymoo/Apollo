7d20ad896e1b56dc437fa3118ca04b9a2fad06a2
Shaarky <shaaravagarwal@gmail.com>
2025-07-17 00:35:11 +0530

    Auto-commit: 2025-07-17 00:35:11

--- codec converter.py
+++ codec converter.py
@@ -8,12 +8,12 @@ for b in a:
     print(b)
     if '.m4a' in b:
         old_file = 'E:\\Shaarav\\wastebin\\' + b
-        b = b.replace('.m4a','.mp3')
+        b = b.replace('.webm','.mp4')
         new_file = 'E:\\Shaarav\\music\\' + b
         try:
             ff = FFmpeg(
                 inputs = {old_file: None},
-                outputs= {new_file:'-ac 2'}
+                outputs= {new_file: None}
             )
             ff.run()
         except Exception as e:
--- ytdlpdownload.py
+++ ytdlpdownload.py
@@ -11,3 +11,5 @@ urls = ['https://youtu.be/jhgVu2lsi_k']
 #PS D:\Projects\Apollo> .\venv\bin\activate
 # yt-dlp -P 'E:\Shaarav\wastebin\' -o '%(title)s - %(channel)s' -x --audio-format 'm4a' 
 # --ffmpeg-location 'C:\ffmpeg' https://www.youtube.com/playlist?list=PLIRFSUH_WlMrZhbyDNoK9o9mNW9XlPXs2
+
+# yt-dlp --embed-subs --sub-langs "en.*" --embed-metadata --embed-thumbnail -f bestaudio+bestvideo --merge-output-format mp4 "https://youtu.be/-rDTRuCOs9g"
