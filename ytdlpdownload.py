00b7f847b569d8f2b66fb38104240f898f217f10
Shaarky <shaaravagarwal@gmail.com>
2025-08-31 17:25:18 +0530

    Auto-commit: 2025-08-31 17:25:17

--- ytdlpdownload.py
+++ ytdlpdownload.py
@@ -7,9 +7,12 @@ urls = ['https://youtu.be/jhgVu2lsi_k']
 #    ydl.download(urls)
     
 #command
-
+#environment
 #PS D:\Projects\Apollo> .\venv\bin\activate
+
+#playlist downloader
 # yt-dlp -P 'E:\Shaarav\wastebin\' -o '%(title)s - %(channel)s' -x --audio-format 'm4a' 
 # --ffmpeg-location 'C:\ffmpeg' https://www.youtube.com/playlist?list=PLIRFSUH_WlMrZhbyDNoK9o9mNW9XlPXs2
 
+#video download with subs
 # yt-dlp --embed-subs --sub-langs "en.*" --embed-metadata --embed-thumbnail -f bestaudio+bestvideo --merge-output-format mp4 "https://youtu.be/-rDTRuCOs9g"
