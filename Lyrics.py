bc50ef10d1e66fc8f5889d584288f6202e723d95
Shaarky <shaaravagarwal@gmail.com>
2025-10-01 23:33:40 +0530

    Auto-commit: 2025-10-01 23:33:40

--- Lyrics.py
+++ Lyrics.py
@@ -27,7 +27,7 @@ def search_lyrics(object,query):
     return song
 
 def update_songs(genius_obj):
-    loc = 'E:\\Shaarav\\playlists\\balanced\\'
+    loc = 'E:\\Shaarav\\playlists\\anime ops\\'
     songlist = []
     errors = []
     for a in os.listdir(loc):
