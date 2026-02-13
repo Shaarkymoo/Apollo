c21f0e8e1d2b3525ffe2d5213d5691894fa54410
Shaarav Agarwal <142115441+Shaarkymoo@users.noreply.github.com>
2026-02-14 00:21:22 +0530

    Auto-commit: 2026-02-14 00:21:22

--- Lyrics.py
+++ Lyrics.py
@@ -86,7 +86,7 @@ def get_existing_lyrics(songfile):
 
 
 def update_songs(genius_obj):
-    loc = r'E:\Shaarav\playlists\drop-beat'
+    loc = r'E:\Shaarav\playlists\hip-hop'
     errors = []
 
     processed = load_processed()
