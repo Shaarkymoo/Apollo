4e0db72de14c6fb52a7a4112df42eca5ebbbc011
Shaarav Agarwal <142115441+Shaarkymoo@users.noreply.github.com>
2026-05-02 04:43:13 +0530

    Auto-commit: 2026-05-02 04:43:13

--- vlcshort.py
+++ vlcshort.py
@@ -7,5 +7,27 @@ VLC_URL = "http://localhost:8080/requests/status.xml"
 def next_track():
     requests.get(VLC_URL, params={"command": "pl_next"}, auth=("", VLC_PASSWORD))
 
-keyboard.add_hotkey("F4+num 6", next_track, suppress=True)
+def prev_track():
+    requests.get(VLC_URL, params={"command": "pl_previous"}, auth=("", VLC_PASSWORD))
+
+def seek_forward():
+    requests.get(VLC_URL, params={"command": "seek", "val": "+5S"}, auth=("", VLC_PASSWORD))
+
+def seek_backward():
+    requests.get(VLC_URL, params={"command": "seek", "val": "-5S"}, auth=("", VLC_PASSWORD))
+
+def play_pause():
+    requests.get(VLC_URL, params={"command": "pl_pause"}, auth=("", VLC_PASSWORD))
+
+# def next_track():
+#     requests.get(VLC_URL, params={"command": "pl_next"}, auth=("", VLC_PASSWORD))
+
+# def next_track():
+#     requests.get(VLC_URL, params={"command": "pl_next"}, auth=("", VLC_PASSWORD))
+
+keyboard.add_hotkey("F4+num 9", next_track, suppress=True)
+keyboard.add_hotkey("F4+num 7", prev_track, suppress=True)
+keyboard.add_hotkey("F4+num 6", seek_forward, suppress=True)
+keyboard.add_hotkey("F4+num 4", seek_backward, suppress=True)
+keyboard.add_hotkey("F4+num 5", play_pause, suppress=True)
 keyboard.wait()
