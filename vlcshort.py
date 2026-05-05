234a5f70ae7366b826f445017ab95d358e573eb1
Shaarav Agarwal <142115441+Shaarkymoo@users.noreply.github.com>
2026-05-05 13:27:04 +0530

    Auto-commit: 2026-05-05 13:27:04

--- vlcshort.py
+++ vlcshort.py
@@ -1,5 +1,6 @@
 import keyboard
 import requests
+import ctypes
 
 VLC_PASSWORD = "vlc"  # change to whatever you set
 VLC_URL = "http://localhost:22123/requests/status.xml"
@@ -22,6 +23,12 @@ def play_pause():
 def toggle_random():
     requests.get(VLC_URL, params={"command": "pl_random"}, auth=("", VLC_PASSWORD))
 
+def close_window():
+    ctypes.windll.user32.PostMessageW(
+        ctypes.windll.user32.GetForegroundWindow(), 
+        0x0010, 0, 0  # WM_CLOSE message
+    )
+
 # def next_track():
 #     requests.get(VLC_URL, params={"command": "pl_next"}, auth=("", VLC_PASSWORD))
 
@@ -32,6 +39,7 @@ def toggle_random():
 #     requests.get(VLC_URL, params={"command": "pl_next"}, auth=("", VLC_PASSWORD))
 
 keyboard.block_key("F4")  # block F4 to prevent conflicts
+keyboard.add_hotkey("alt+F4", close_window, suppress=True)
 keyboard.add_hotkey("F4+num 9", next_track, suppress=True)
 keyboard.add_hotkey("F4+num 8", toggle_random, suppress=True)
 keyboard.add_hotkey("F4+num 7", prev_track, suppress=True)
