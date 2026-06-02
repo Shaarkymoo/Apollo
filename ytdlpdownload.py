7f147197e71aca5688b938daf787428ca3eb2ff4
Shaarav Agarwal <142115441+Shaarkymoo@users.noreply.github.com>
2026-06-02 11:56:51 +0530

    Auto-commit: 2026-06-02 11:56:51

--- ytdlpdownload.py
+++ ytdlpdownload.py
@@ -3,16 +3,27 @@ from yt_dlp import YoutubeDL
 
 urls = ['https://youtu.be/jhgVu2lsi_k']
 
-#with YoutubeDL() as ydl:
-#    ydl.download(urls)
+"""
+with YoutubeDL() as ydl:
+   ydl.download(urls)
     
-#command
-#environment
-#PS D:\Projects\Apollo> .\venv\bin\activate
+command
+environment
+PS D:\Projects\Apollo> .\venv\bin\activate
 
-#playlist downloader
-# yt-dlp -P 'E:\Shaarav\wastebin\' -o '%(title)s - %(channel)s' -x --audio-format 'm4a' 
-# --ffmpeg-location 'C:\ffmpeg' https://www.youtube.com/playlist?list=PLIRFSUH_WlMrZhbyDNoK9o9mNW9XlPXs2
+playlist downloader
+yt-dlp -P 'E:\Shaarav\wastebin\' -o '%(title)s - %(channel)s' -x --audio-format 'm4a' 
+--ffmpeg-location 'C:\ffmpeg' https://www.youtube.com/playlist?list=PLIRFSUH_WlMrZhbyDNoK9o9mNW9XlPXs2
 
-#video download with subs
-# yt-dlp --embed-subs --sub-langs "en.*" --embed-metadata --embed-thumbnail -f bestaudio+bestvideo --merge-output-format mp4 "https://youtu.be/-rDTRuCOs9g"
+video download with subs
+yt-dlp --embed-subs --sub-langs "en.*" --embed-metadata --embed-thumbnail -f bestaudio+bestvideo --merge-output-format mp4 "https://youtu.be/-rDTRuCOs9g"
+
+yt-dlp \
+  --split-chapters \
+  --force-keyframes-at-cuts \
+  -x \
+  --audio-format mp3 \
+  -o "%(title)s/%(section_number)02d - %(section_title)s.%(ext)s" \
+  "YOUTUBE_URL"
+
+"""
